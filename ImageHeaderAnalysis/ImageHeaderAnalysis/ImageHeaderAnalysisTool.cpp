ef23b9c72c701dda960fedb83498a1e36c9bcb08
Elysia <zqynihan@gmail.com>
2025-06-15 12:55:40 +0800

    文件分离

--- ImageHeaderAnalysis/ImageHeaderAnalysis/ImageHeaderAnalysisTool.cpp
+++ ImageHeaderAnalysis/ImageHeaderAnalysis/ImageHeaderAnalysisTool.cpp
@@ -1,5 +1,45 @@
 ﻿#include "ImageHeaderAnalysisTool.h"
 
+ImageSize ImageHeaderAnalysisTool::GetImageSizeByHeaderInfo(const std::string& filename)
+{
+    std::ifstream file(filename, std::ios::binary);
+    if (!file.is_open())
+    {
+        return { -1, -1 };
+    }
+
+    // 文件类型判断, 暂时只需要 4 个字节.
+    std::vector<unsigned char> header(4);
+    file.read(reinterpret_cast<char*>(header.data()), header.size());
+
+    // JPG 图片.
+    if (header[0] == 0xFF && header[1] == 0xD8)
+    {
+        return GetJPGImageSizeByHeaderInfo(file, header);
+    }
+
+    // GIF 图片.
+    if (header[0] == 'G' && header[1] == 'I' && header[2] == 'F')
+    {
+        return GetGIFImageSizeByHeaderInfo(file);
+    }
+
+    // PNG 图片.
+    if (header[0] == 0x89 && header[1] == 'P' && header[2] == 'N' && header[3] == 'G')
+    {
+        return GetPNGImageSizeByHeaderInfo(file);
+    }
+
+    // BMP 图片.
+    if (header[0] == 'B' && header[1] == 'M')
+    {
+        return GetBMPImageSizeByHeaderInfo(file);
+    }
+
+    file.close();
+    return { -1, -1 };
+}
+
 ImageSize ImageHeaderAnalysisTool::GetJPGImageSizeByHeaderInfo(std::ifstream& file, const std::vector<unsigned char>& header)
 {
     // JPEG
